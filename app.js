4a53e16f8ac31278d50ff4acf317f3c4e5bab350
Dmitry Shiryaev <hyrtou@gmail.com>
2024-08-22 22:27:00 +0300

    add switch case

--- app.js
+++ app.js
@@ -3,44 +3,79 @@
 const resultElement = document.getElementById("result");
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
-const plussBtn = document.getElementById("pluss");
-const minusBtn = document.getElementById("minus");
-const multiplicationBtn = document.getElementById("multiplication");
-const separationBtn = document.getElementById("separation");
+// const plussBtn = document.getElementById("pluss");
+// const minusBtn = document.getElementById("minus");
+// const multiplicationBtn = document.getElementById("multiplication");
+// const separationBtn = document.getElementById("separation");
 const computeBtn = document.getElementById("compute");
-let press = "+";
+let plusBtn = "+";
+let minusBtn = "-";
+let multiplicationBtn = "*";
+let separationBtn = "/";
 
-plussBtn.onclick = function () {
-  press = "+";
-};
+// plussBtn.onclick = function () {
+//   pressBtn = "+";
+// };
 
-minusBtn.onclick = function () {
-  press = "-";
-};
+// minusBtn.onclick = function () {
+//   pressBtn = "-";
+// };
 
-multiplicationBtn.onclick = function () {
-  press = "*";
-};
+// multiplicationBtn.onclick = function () {
+//   pressBtn = "*";
+// };
 
-separationBtn.onclick = function () {
-  press = "/";
-};
+// separationBtn.onclick = function () {
+//   pressBtn = "/";
+// };
+
+// computeBtn.onclick = function () {
+//   if (press == "+") {
+//     const sum = Number(input1.value) + Number(input2.value);
+//     resultElement.textContent = sum;
+//   } else if (press == "-") {
+//     const sum = Number(input1.value) - Number(input2.value);
+//     resultElement.textContent = sum;
+//   } else if (press == "*") {
+//     const sum = Number(input1.value) * Number(input2.value);
+//     resultElement.textContent = sum;
+//   } else if (press == "/") {
+//     const sum = Number(input1.value) / Number(input2.value);
+//     resultElement.textContent = sum;
+//   }
+// };
 
 computeBtn.onclick = function () {
-  if (press == "+") {
-    const sum = Number(input1.value) + Number(input2.value);
-    resultElement.textContent = sum;
-  } else if (press == "-") {
-    const sum = Number(input1.value) - Number(input2.value);
-    resultElement.textContent = sum;
-  } else if (press == "*") {
-    const sum = Number(input1.value) * Number(input2.value);
-    resultElement.textContent = sum;
-  } else if (press == "/") {
-    const sum = Number(input1.value) / Number(input2.value);
-    resultElement.textContent = sum;
+  switch ((plusBtn, minusBtn, multiplicationBtn, separationBtn)) {
+    case "plusBtn":
+      resultElement.textContent = Number(input1.value) + Number(input2.value);
+      break;
+
+    case "-":
+      resultElement.textContent = Number(input1.value) - Number(input2.value);
+      break;
+
+    case "*":
+      resultElement.textContent = Number(input1.value) * Number(input2.value);
+      break;
+
+    case "/":
+      resultElement.textContent = Number(input1.value) / Number(input2.value);
+      break;
   }
 };
+
+// case "-":
+//   resultElement.textContent = Number(input1.value) - Number(input2.value);
+//   break;
+
+// case "*":
+//   resultElement.textContent = Number(input1.value) / Number(input2.value);
+//   break;
+
+// case "/":
+//   resultElement.textContent = Number(input1.value) * Number(input2.value);
+//   break;
 
 // Знаю что в конце должен быть просто else без if но почему то не работает(
 // В звонке можем оптимизорвать код. Мне кажется. его можно сделать поменьше
